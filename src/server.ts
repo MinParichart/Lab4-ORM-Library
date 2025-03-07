10a18b976ef1550eef01eb316c7a8ceee58b48fb
MinChatr <min_etc@hotmail.com>
2025-03-07 13:01:03 +0700

    Lab 3 - Task 10- Adjust Endpoint for Upload File and Receive Output URL

--- src/server.ts
+++ src/server.ts
@@ -273,9 +273,9 @@ app.post('/upload', upload.single('file'), async (req: any, res: any) => { // PO
       const bucket = 'image_library';
       const filePath = `uploads_library/${file.originalname}`;
    
-      await uploadFile(bucket, filePath, file);
+      const outputUrl = await uploadFile(bucket, filePath, file);
   
-      res.status(200).send('File uploaded successfully.');
+      res.status(200).send(outputUrl);
     } catch (error) {
       res.status(500).send('Error uploading file.');
     }
